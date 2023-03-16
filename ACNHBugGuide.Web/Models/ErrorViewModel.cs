c515249ddbd75467ecb7bf56e56a5fc964c7e37d
Kaleb Garrett <kalebagarrett@gmail.com>
2023-03-16 13:03:57 -0400

    Created web project

---
+++ ACNHBugGuide.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ACNHBugGuide.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
