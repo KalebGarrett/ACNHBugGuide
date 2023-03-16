2e4c310db4c8b6b3e1ea4c662f31275099050e17
Kaleb Garrett <kalebagarrett@gmail.com>
2023-03-16 13:18:18 -0400

    Removed web project

--- ACNHBugGuide.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ACNHBugGuide.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
